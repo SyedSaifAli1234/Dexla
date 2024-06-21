ac8e1b928991f6308cfd7d975ff99da36988b45f
SyedSaifAli1234 <saif.ali1000@gmail.com>
2024-06-22 00:53:41 +0500

    publication file

--- src/pages/actualities.jsx
+++ src/pages/actualities.jsx
@@ -40,21 +40,21 @@ export function Actualities() {
                 />
                 <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
             </section>
-            <section className="relative bg-white py-16 px-4">
-                <div className="container mx-auto max-w-4xl">
-                    <div className="relative mb-6 -mt-64 flex w-full min-w-0 flex-col break-words rounded-3xl bg-white shadow-xl shadow-gray-500/5">
-                        <div className="px-6 py-12">
-                            <div className="rounded overflow-hidden shadow-lg mb-10">
-                                <div className="flex flex-col">
-                                    <img className="w-30 object-cover" src={publicationImage} alt="Publication" />
-                                    <div className="p-6">
+            <section className="relative bg-white py-8 px-4">
+                <div className="container mx-auto max-w-sm">
+                    <div className="relative mb-6 flex w-full min-w-0 flex-col break-words rounded-3xl bg-white shadow-xl shadow-gray-500/5">
+                        <div className="px-4 py-6">
+                            <div className="rounded overflow-hidden shadow-lg mb-4">
+                                <div className="flex flex-col items-center">
+                                    <img className="w-full h-auto max-w-xs object-cover" src={publicationImage} alt="Publication" />
+                                    <div className="p-4 text-center">
                                         <Typography variant="h6" color="blue-gray" className="font-bold mb-2">
                                             PUBLICATION
                                         </Typography>
-                                        <Typography variant="body1" color="blue-gray" className="flex items-center mb-4 text-gray-600">
+                                        <Typography variant="body1" color="blue-gray" className="flex justify-center items-center mb-2 text-gray-600">
                                             <span className="text-gray-600 mr-2">👤 Nicolas</span> | <span className="text-gray-600 ml-2">📅 21 December 2023</span>
                                         </Typography>
-                                        <Typography variant="h5" color="blue-gray" className="font-bold mb-4">
+                                        <Typography variant="h5" color="blue-gray" className="font-bold mb-2">
                                             Comprendre le rôle essentiel d'un avocat dans le système juridique
                                         </Typography>
                                         <Typography variant="body2" color="blue-gray" className="mb-2">
